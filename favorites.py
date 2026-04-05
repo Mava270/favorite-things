d5c1868dbb7acf67d7e13a1bc39f41df6b22903a
Mava270 <vmava73@gmail.com>
2026-04-05 16:44:00 +0200

    Add new favorite item to the list

--- favorites.py
+++ favorites.py
@@ -3,3 +3,4 @@ favorites = ["Python", "Coffee", "Music", "Books"]
 print("My favorite things:")
 for item in favorites:
     print("-", item)
+favorites = ["Python", "Coffee", "Music", "Books", "Gaming"]
